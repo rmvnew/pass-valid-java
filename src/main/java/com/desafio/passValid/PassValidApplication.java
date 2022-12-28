ce2169b5da62f92f06538dd14f3829831672dff4
Ricardo <rmvnew@gmail.com>
2022-12-28 08:04:05 -0400

    first commit

---
+++ src/main/java/com/desafio/passValid/PassValidApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.passValid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassValidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassValidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/passValid/PassValidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.passValid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassValidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
